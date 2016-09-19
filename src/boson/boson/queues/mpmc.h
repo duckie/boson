20f785b8922528969cde7174d5d70b6f12dbffcf
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2016-09-19 13:36:46 +0200

    Add unbounded_mpmc wrapper.

--- src/boson/boson/queues/mpmc.h
+++ src/boson/boson/queues/mpmc.h
@@ -50,6 +50,46 @@ class bounded_mpmc {
   };
 };
 
+/**
+ * unbounded_mpmc is a MPMC unbouded queue
+ *
+ * Current implementation is just a wrapper around
+ * a dynamic folly::MPMCQueue 
+ *
+ * We decide a push that modifies the capacity can be blocking here, we dont care
+ * since such calls will not scale
+ */
+template <class ContentType>
+class unbounded_mpmc {
+  using content_t = ContentType;
+  folly::MPMCQueue<ContentType, std::atomic, true> queue_;
+
+ public:
+  using content_type = ContentType;
+  unbounded_mpmc(size_t initial_capacity = 1)
+    : queue_(initial_capacity)
+  {}
+  unbounded_mpmc(unbounded_mpmc const&) = delete;
+  unbounded_mpmc(unbounded_mpmc&&) = default;
+  unbounded_mpmc& operator=(unbounded_mpmc const&) = delete;
+  unbounded_mpmc& operator=(unbounded_mpmc&&) = default;
+  ~unbounded_mpmc() = default;
+
+  template <class ... Args>
+  bool push(Args&& ... args) {
+    queue_.blockingWrite(std::forward<Args>(args)...);
+    return true;
+  };
+
+  bool pop(content_type& element) {
+    return queue_.read(element);
+  };
+
+  bool blocking_pop(content_type& element) {
+    return queue_.blockingRead(element);
+  };
+};
+
 };  // namespace queues
 };  // namespace boson
 
--- src/boson/test/queues_mpmc.cc
+++ src/boson/test/queues_mpmc.cc
@@ -7,7 +7,18 @@
 
 using namespace boson::queues;
 
-TEST_CASE("Queues - MPMPC - simple push/pop", "[queues][mpmc]") {
+TEST_CASE("Queues - Bounded MPMPC - simple push/pop", "[queues][bounded_mpmc]") {
+  bounded_mpmc<int> test(1);
+  bool attempt1 = test.push(1);
+  bool attempt2 = test.push(1);
+  CHECK(attempt1);
+  CHECK(!attempt2);
+  int result = 0;
+  test.pop(result);
+  CHECK(result == 1);
+}
+
+TEST_CASE("Queues - Bounded MPMPC - serial random integers", "[queues][bounded_mpmc]") {
   bounded_mpmc<int> test(10);
   test.push(1);
   int result = 0;
@@ -15,8 +26,8 @@ TEST_CASE("Queues - MPMPC - simple push/pop", "[queues][mpmc]") {
   CHECK(result == 1);
 }
 
-TEST_CASE("Queues - MPMPC - serial random integers", "[queues][mpmc]") {
-  bounded_mpmc<int> test(10);
+TEST_CASE("Queues - Unbounded MPMPC - serial random integers", "[queues][unbounded_mpmc]") {
+  unbounded_mpmc<int> test(10);
   test.push(1);
   int result = 0;
   test.pop(result);
