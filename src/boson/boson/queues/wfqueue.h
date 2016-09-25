141ccdaa5ccadb1ce944bf6a9bcb4981f77eded4
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2016-09-26 01:48:17 +0200

    Wip

--- src/boson/boson/queues/wfqueue.h
+++ src/boson/boson/queues/wfqueue.h
@@ -3,12 +3,19 @@
 #include <atomic>
 #include <cstdint>
 #include <type_traits>
+#include <cassert>
 
 namespace boson {
 namespace queues {
 
-static constexpr size_t PAGE_SIZE = 4096;
-static constexpr size_t CACHE_LINE_SIZE = 64;
+static constexpr std::size_t PAGE_SIZE = 4096;
+static constexpr std::size_t CACHE_LINE_SIZE = 64;
+static constexpr std::size_t MAX_SPIN = 100;
+static constexpr int MAX_PATIENCE = 10;
+static constexpr void * BOT = 0;
+//static constexpr void * TOP = reinterpret_cast<void*>(-1);
+//#define BOT ((void *) 0)
+#define TOP ((void *)-1)
 
 /**
  * Wfqueue is a wait-free MPMC concurrent queue
@@ -22,43 +29,56 @@ class alignas(2 * CACHE_LINE_SIZE) wfqueue {
   //#define CACHE_ALIGNED __attribute__((aligned(CACHE_LINE_SIZE)))
   //#define DOUBLE_CACHE_ALIGNED __attribute__((aligned(2 * CACHE_LINE_SIZE)))
   using content_t = ContentType;
-  static constexpr size_t WFQUEUE_NODE_SIZE = ((1 << 10) - 2);
+  static constexpr std::size_t WFQUEUE_NODE_SIZE = ((1 << 10) - 2);
   static constexpr std::memory_order const order_relaxed = std::memory_order::memory_order_relaxed;
   static constexpr std::memory_order const order_acquire = std::memory_order::memory_order_acquire;
   static constexpr std::memory_order const order_release = std::memory_order::memory_order_release;
 
-  struct enq_t alignas(CACHE_LINE_SIZE) {
-    long volatile id;
-    void* volatile val;
+  struct alignas(CACHE_LINE_SIZE) enq_t {
+    std::atomic<long> volatile id;
+    std::atomic<void*> volatile val;
   };
 
-  struct deq_t alignas(CACHE_LINE_SIZE) {
-    long volatile id;
-    long volatile idx;
+  struct alignas(CACHE_LINE_SIZE) deq_t {
+    std::atomic<long> volatile id;
+    std::atomic<long> volatile idx;
   };
 
   struct cell_t {
-    void* volatile val;
-    struct enq_t* volatile enq;
-    struct deq_t* volatile deq;
+    std::atomic<ContentType> volatile val;
+    std::atomic<enq_t>* volatile enq;
+    std::atomic<deq_t*> volatile deq;
     void* pad[5];
   };
 
   struct node_t {
-    alignas(CACHE_LINE_SIZE) node_t* volatile next;
+    alignas(CACHE_LINE_SIZE) std::atomic<node_t*> volatile next;
     alignas(CACHE_LINE_SIZE) long id;
     alignas(CACHE_LINE_SIZE) cell_t cells[WFQUEUE_NODE_SIZE];
   };
 
-  alignas(2 * CACHE_LINE_SIZE) volatile long Ei;
-  alignas(2 * CACHE_LINE_SIZE) volatile long Di;
-  alignas(2 * CACHE_LINE_SIZE) volatile long Hi;
-  struct _node_t* volatile Hp;
+  struct handle_t {
+    handle_t * next;
+    std::atomic<node_t*> volatile Hp;
+    std::atomic<node_t*> volatile Ep;
+    std::atomic<node_t*> volatile Dp;
+    alignas(CACHE_LINE_SIZE) enq_t Er;
+    alignas(CACHE_LINE_SIZE) deq_t Dr;
+    alignas(CACHE_LINE_SIZE) handle_t * Eh;
+    long Ei;
+    handle_t * Dh;
+    alignas(CACHE_LINE_SIZE) node_t * spare;
+    int delay;
+  };
+
+  alignas(2 * CACHE_LINE_SIZE) volatile std::atomic<long> Ei;
+  alignas(2 * CACHE_LINE_SIZE) volatile std::atomic<long> Di;
+  alignas(2 * CACHE_LINE_SIZE) volatile std::atomic<long> Hi;
+  std::atomic<node_t*> volatile Hp;
   long nprocs;
 
  public:
   using content_type = ContentType;
-  static constexpr std::size_t const size = Size;
 
   wfqueue() noexcept(std::is_nothrow_default_constructible<ContentType>()) = default;
   wfqueue(wfqueue const&) = delete;
@@ -66,6 +86,99 @@ class alignas(2 * CACHE_LINE_SIZE) wfqueue {
   wfqueue& operator=(wfqueue const&) = delete;
   wfqueue& operator=(wfqueue&&) noexcept(false) = default;
   ~wfqueue() = default;
+
+  void enqueue(handle_t * th, void * v)
+  {
+    th->Hp = th->Ep.load(std::memory_order::memory_order_relaxed);
+
+    long id;
+    int p = MAX_PATIENCE;
+    while (!this->enq_fast(th, v, &id) && p-- > 0);
+    if (p < 0) 
+      this->enq_slow(th, v, id);
+    th->Hp.store(nullptr,std::memory_order_release);
+  }
+
+  inline node_t* new_node() {
+    node_t* n = nullptr;
+    int ret = posix_memalign(&n, PAGE_SIZE, sizeof(node_t));
+    assert(ret == 0);
+    memset(n, 0, sizeof(node_t));
+    return n;
+  }
+
+  cell_t* find_cell(std::atomic<node_t*>* ptr, long i, handle_t* th) {
+    node_t* curr = ptr->load(std::memory_order::memory_order_relaxed);
+    long j;
+    for (j = curr->id; j < i / WFQUEUE_NODE_SIZE; ++j) {
+      node_t* next = curr->next->load(std::memory_order::memory_order_relaxed);
+      if (next == nullptr) {
+        node_t* temp = th->spare;
+        if (!temp) {
+          temp = new_node();
+          th->spare = temp;
+        }
+        temp->id = j + 1;
+        if (curr->next.compare_exchange_strong(&next, temp, std::memory_order::memory_order_release,
+                                               std::memory_order::memory_order_acquire)) {
+          next = temp;
+          th->spare = nullptr;
+        }
+      }
+      curr = next;
+    }
+
+    ptr->store(curr, std::memory_order_relaxed);  // Hum
+    return &curr->cells[i % WFQUEUE_NODE_SIZE];
+  }
+
+  int enq_fast(handle_t * th, void * v, long * id)
+  {
+    long i = this->Ei.fetch_add(1,std::memory_order_seq_cst);
+    cell_t * c = find_cell(&th->Ep, i, th);
+    void * cv = BOT;
+
+    if (c->val.compare_exchange_strong(cv, v, std::memory_order::memory_order_relaxed,
+                                       std::memory_order::memory_order_relaxed)) {
+      return 1;
+    } else {
+      *id = i;
+      return 0;
+    }
+  }
+  void enq_slow(handle_t* th, void* v, long id) {
+    enq_t* enq = th->Er.load(std::memory_order::memory_order_relaxed);
+    enq->val.store(v,std::memory_order::memory_order_relaxed);  // hum
+    enq->id.store(id,std::memory_order::memory_order_release);
+
+    node_t* tail = th->Ep.load(std::memory_order::memory_order_relaxed);
+    long i = 0;
+    cell_t* c = nullptr;
+
+    do {
+      i = this->Ei.fetch_add(1,std::memory_order::memory_order_relaxed);
+      c = find_cell(&tail, i, th);
+      enq_t* ce = BOT;
+
+      if (c->enq.compare_exchange_strong(ce, enq) && c->val.load(std::memory_order::memory_order_relaxed) != TOP) {
+        if (enq->id.compare_exchange_strong(id, -i, std::memory_order::memory_order_relaxed,
+                                            std::memory_order::memory_order_relaxed))
+          id = -i;
+        break;
+      }
+    } while (enq->id.load(std::memory_order::memory_order_relaxed) > 0);
+
+    id = -enq->id;
+    c = find_cell(&th->Ep, id, th);
+    if (id > i) {
+      long Ei = this->Ei.load(std::memory_order::memory_order_relaxed); // hum
+      while (Ei <= id &&
+             !this->Ei.compare_exchange_strong(&Ei, id + 1, std::memory_order::memory_order_relaxed,
+                                               std::memory_order::memory_order_relaxed))
+        ;
+    }
+    c->val.stor(v,std::memory_order_relaxed);
+  }
 };
 
 };  // namespace queues
---
+++ src/boson/test/test_wfqueue.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <limits>
+#include <random>
+#include <thread>
+#include "boson/queues/wfqueue.h"
+#include "catch.hpp"
+
+TEST_CASE("Queues - WeakRB - serial random integers", "[queues][wfqueue]") {
+  boson::queues::wfqueue<int> queue();
+  queue.enqueue(1);
+}
