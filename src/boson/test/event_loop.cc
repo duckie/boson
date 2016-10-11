b2289e7e1b7e5d2d690635dad48d357074ed6df6
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2016-10-12 00:39:32 +0200

    Bugfix wip

--- src/boson/test/event_loop.cc
+++ src/boson/test/event_loop.cc
@@ -2,25 +2,25 @@
 #include <unistd.h>
 #include <thread>
 #include "catch.hpp"
+#include <cstdio>
 
 struct handler01 : public boson::event_handler {
   int last_id{0};
-  int last_fd{-1};
+  int last_read_fd{-1};
+  int last_write_fd{-1};
   void* last_data{nullptr};
 
   void event(int event_id, void* data) override {
     last_id = event_id;
-    last_fd = -1;
     last_data = data;
   }
   void read(int fd, void* data) override {
-    last_id = -1;
-    last_fd = fd;
+    last_read_fd = fd;
     last_data = data;
   }
   void write(int fd, void* data) override {
     last_id = -1;
-    last_fd = fd;
+    last_write_fd = fd;
     last_data = data;
   }
 };
@@ -50,13 +50,48 @@ TEST_CASE("Event Loop - FD Read/Write", "[eventloop][read/write]") {
   loop.register_write(pipe_fds[1], nullptr);
 
   loop.loop(1);
-  CHECK(handler_instance.last_fd == pipe_fds[1]);
+  CHECK(handler_instance.last_write_fd == pipe_fds[1]);
   CHECK(handler_instance.last_data == nullptr);
 
   size_t data{1};
   ::write(pipe_fds[1], &data, sizeof(size_t));
 
   loop.loop(1);
-  CHECK(handler_instance.last_fd == pipe_fds[0]);
+  CHECK(handler_instance.last_read_fd == pipe_fds[0]);
   CHECK(handler_instance.last_data == nullptr);
+}
+
+TEST_CASE("Event Loop - FD Read/Write same FD", "[eventloop][read/write]") {
+  FILE* tmp_file = std::tmpfile();
+  int fd = ::fileno(tmp_file);
+  // Write some data
+  ::fputs("data",tmp_file);
+  ::fseek(tmp_file,0,0);
+
+  // Check independant events
+
+  handler01 handler_instance;
+  boson::event_loop loop(handler_instance);
+  //int event_read = loop.register_read(fd, nullptr);
+  loop.loop(1);
+  //CHECK(handler_instance.last_read_fd == fd);
+
+
+  //int pipe_fds[2];
+  //::pipe(pipe_fds);
+//
+  //boson::event_loop loop(handler_instance);
+  //loop.register_read(pipe_fds[0], nullptr);
+  //loop.register_write(pipe_fds[1], nullptr);
+//
+  //loop.loop(1);
+  //CHECK(handler_instance.last_fd == pipe_fds[1]);
+  //CHECK(handler_instance.last_data == nullptr);
+//
+  //size_t data{1};
+  //::write(pipe_fds[1], &data, sizeof(size_t));
+//
+  //loop.loop(1);
+  //CHECK(handler_instance.last_fd == pipe_fds[0]);
+  //CHECK(handler_instance.last_data == nullptr);
 }
