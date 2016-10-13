4bbb45847417017337330cd184f49730a19e6db3
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2016-10-13 18:16:44 +0200

    Continue refactoring

---
+++ src/boson/boson/system.h
@@ -0,0 +1,43 @@
+#ifndef BOSON_SYSTEM_H_
+#define BOSON_SYSTEM_H_
+
+#ifdef WINDOWS
+#include <winsock2.h> 
+#else
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
+//#define closesocket(s) close(s)
+typedef int SOCKET;
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+typedef struct in_addr IN_ADDR;
+#endif
+
+#include <chrono>
+#include <cstdint>
+
+namespace boson {
+#ifdef WINDOWS
+using fd_t = HANDLE;
+using sockaddr_in = SOCKADDR_IN;
+using sockaddr = SOCKADDR;
+using in_addr = IN_ADDR;
+#else
+
+static constexpr int const INVALID_SOCKET = -1;
+static constexpr int const SOCKET_ERROR = -1;
+using fd_t = int;
+using socket_t = int;
+using sockaddr_in = struct ::sockaddr_in;
+using sockaddr = struct ::sockaddr;
+using in_addr = struct ::in_addr;
+
+#endif
+}  // namespace boson
+
+#endif  // BOSON_SYSCALLS_H_
