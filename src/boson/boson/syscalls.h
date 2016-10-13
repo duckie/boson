4b3d74cc4771edaf55ca26ca707e50a14333144d
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2016-10-13 18:16:16 +0200

    Start a refactoring.

--- src/boson/boson/syscalls.h
+++ src/boson/boson/syscalls.h
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <chrono>
 #include <cstdint>
+#include "system.h"
 
 namespace boson {
 
@@ -23,16 +24,16 @@ void sleep(std::chrono::milliseconds duration);
 /**
  * Boson equivalent to POSIX read system call
  */
-ssize_t read(int fd, void *buf, size_t count);
+ssize_t read(fd_t fd, void *buf, size_t count);
 
 /**
  * Boson equivalent to POSIX write system call
  */
-ssize_t write(int fd, const void *buf, size_t count);
+ssize_t write(fd_t fd, const void *buf, size_t count);
 
-int accept(int socket, struct sockaddr *address, socklen_t *address_len);
-size_t send(int socket, const void *buffer, size_t length, int flags);
-ssize_t recv(int socket, void *buffer, size_t length, int flags);
+socket_t accept(socket_t socket, sockaddr *address, socklen_t *address_len);
+size_t send(socket_t socket, const void *buffer, size_t length, int flags);
+ssize_t recv(socket_t socket, void *buffer, size_t length, int flags);
 
 }  // namespace boson
 
