6887bbf3f7393d5039f1a2c48af8b0cc1110f306
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2017-02-06 12:47:47 +0100

    Add counters for the test client.

--- docs/go_examples/simplechat/client.go
+++ docs/go_examples/simplechat/client.go
@@ -2,32 +2,60 @@ package main
 
 import "net"
 import "fmt"
+import "sync/atomic"
+import "time"
 
-func clientRead(conn net.Conn) {
+func clientRead(conn net.Conn, counter *uint64) {
   fmt.Println("Start read")
-  buf := make([]byte, 2048)
   for {
-    conn.Read(buf)
+    buf := make([]byte, 2048)
+    nread, err := conn.Read(buf)
+    if (err == nil && 0 < nread) {
+      atomic.AddUint64(counter,1)
+    }
   }
 }
 
-func clientWrite(conn net.Conn) {
+func clientWrite(conn net.Conn, counter *uint64) {
   fmt.Println("Start write")
   for {
-    conn.Write([]byte("message"))
+    nwritten, err := conn.Write([]byte("message"))
+    if (err == nil && 0 < nwritten) {
+      atomic.AddUint64(counter,1)
+    }
   }
 }
 
 func main() {
+  nb_iter := 10
   i := 0
-  for i < 10 {
+  wcounters := make([]uint64,nb_iter)
+  rcounters := make([]uint64,nb_iter)
+  for i < nb_iter {
+    rcounters[i] = 0
+    wcounters[i] = 0
+    i++
+  }
+  i=0
+  for i < nb_iter {
     conn, err := net.Dial("tcp", "127.0.0.1:8080")
     if (err == nil) {
-      go clientWrite(conn)
-      go clientRead(conn)
+      go clientWrite(conn,&wcounters[i])
+      go clientRead(conn,&rcounters[i])
       i++
     }
   }
+
+  go func(rcounters []uint64, wcounters []uint64, size int) {
+    for {
+      for i := 0; i < nb_iter; i++ {
+        fmt.Printf("(%9d/%9d) ", atomic.LoadUint64(&rcounters[i]), atomic.LoadUint64(&wcounters[i]))
+      }
+      fmt.Printf("\n") 
+      time.Sleep(1*time.Second)
+    }
+  }(rcounters,wcounters,nb_iter)
+
   fmt.Println("Finished launching clients")
   blocker := make(chan int)
   <- blocker
