9b2b79d8e581486893cb08d5b17646d3288e4400
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2017-05-01 18:36:26 +0000

    Add existing syscalls to shim lib.

--- src/boson/src/syscall_shim.cc
+++ src/boson/src/syscall_shim.cc
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <sys/syscall.h>
 #include <dlfcn.h>
 #include <stdio.h>
@@ -16,9 +17,17 @@ namespace internal {
 #define EXPAND_AS_ARGS_1(T1,V1) V1
 #define EXPAND_AS_FUNC_1(T1,V1) T1 V1
 
+#define EXPAND_AS_TYPE_PIPE1(T1,V1) T1[2]
+#define EXPAND_AS_ARGS_PIPE1(T1,V1) V1
+#define EXPAND_AS_FUNC_PIPE1(T1,V1) T1 V1[2]
+
 #define EXPAND_AS_TYPE_2(T1,V1,T2,V2) T1,T2
 #define EXPAND_AS_ARGS_2(T1,V1,T2,V2) V1,V2
 #define EXPAND_AS_FUNC_2(T1,V1,T2,V2) T1 V1,T2 V2
+
+#define EXPAND_AS_TYPE_PIPE2(T1,V1,T2,V2) T1[2],T2
+#define EXPAND_AS_ARGS_PIPE2(T1,V1,T2,V2) V1,V2
+#define EXPAND_AS_FUNC_PIPE2(T1,V1,T2,V2) T1 V1[2],T2 V2
 
 #define EXPAND_AS_TYPE_3(T1,V1,T2,V2,T3,V3) T1,T2,T3
 #define EXPAND_AS_ARGS_3(T1,V1,T2,V2,T3,V3) V1,V2,V3
@@ -50,4 +59,11 @@ RETURN_TYPE SYMBOL(EXPAND_AS_FUNC_##NB_ARGS( __VA_ARGS__)) { \
 
 DECLARE_SYSTEM_SYMBOL_SHIM(open,int,3,const char *,pathname,int,flags,mode_t,mode);
 DECLARE_SYSTEM_SYMBOL_SHIM(creat,int,2,const char *,pathname,mode_t,mode);
+DECLARE_SYSTEM_SYMBOL_SHIM(pipe,int,PIPE1,int,fds);
+DECLARE_SYSTEM_SYMBOL_SHIM(pipe2,int,PIPE2,int,fds,int,flags);
 DECLARE_SYSTEM_SYMBOL_SHIM(socket,int,3,int,domain,int,type,int,protocol);
+DECLARE_SYSTEM_SYMBOL_SHIM(read,ssize_t,3,int,fd,void*,buffer,size_t,count);
+DECLARE_SYSTEM_SYMBOL_SHIM(write,ssize_t,3,int,fd,const void*,buffer,size_t,count);
+DECLARE_SYSTEM_SYMBOL_SHIM(accept,int,3,int,fd,sockaddr*,address,socklen_t*,count);
+DECLARE_SYSTEM_SYMBOL_SHIM(send,ssize_t,4,int,fd,const void*,buffer,size_t,count,int,flags);
+DECLARE_SYSTEM_SYMBOL_SHIM(recv,ssize_t,4,int,fd,void*,buffer,size_t,count,int,flags);
